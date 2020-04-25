19f51ffa8bd6edfec8be7f80245cd5fc3d8ced2e
Aarsh Patel <30925283+Aarsh4@users.noreply.github.com>
2020-04-25 12:53:22 +0530

    Update ufw.sh

--- ufw.sh
+++ ufw.sh
@@ -1,15 +1,12 @@
 ipfile="/root/ufw_country_allow/country_ipblocks.txt"
 serverport="27015"
 protocol="udp"
-
-echo "Allowing SSH Port" 
+echo "Allowing SSH Port"
 ufw allow ssh 
-echo "Allowing your CIDR ipblocks to $serverport/$protocol"
-
+echo "Allowing your CIDR ipblocks to $serverport $protocol"
 for i in $(cat $ipfile); do
 	sudo ufw allow proto $protocol from $i to any port $serverport
 done
-
 echo "Enabling UFW Firewall :)"
 ufw --force enable 
 ufw reload 
